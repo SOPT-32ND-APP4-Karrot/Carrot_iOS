7e66701dcf58a003ce962dc609c82bf78c9f9ca1
ckkim817 <ckkim817@konkuk.ac.kr>
2023-05-15 10:11:03 +0900

    Add [#3] 프로젝트 생성

---
+++ SOPT-32nd-APP4-CarrotiOS/SOPT-32nd-APP4-CarrotiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SOPT-32nd-APP4-CarrotiOS
+//
+//  Created by kyun on 2023/05/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
