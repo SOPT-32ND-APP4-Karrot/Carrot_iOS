6a511b48ebfcb2adf7c3b4d49dabd8de813aef78
ckkim817 <ckkim817@konkuk.ac.kr>
2023-05-15 12:07:26 +0900

    Add [#3] 컬러 에셋 추가, 컬러 세팅

---
+++ SOPT-32nd-APP4-CarrotiOS/SOPT-32nd-APP4-CarrotiOS/Common/Consts/Color.swift
@@ -0,0 +1,29 @@
+//
+//  Color.swift
+//  SOPT-32nd-APP4-CarrotiOS
+//
+//  Created by kyun on 2023/05/15.
+//
+
+import UIKit
+
+public enum Color {
+    
+    public static let carrotOrange = UIColor(named: "1. Orange")
+    public static let carrotLightOrange = UIColor(named: "2. Light Orange")
+    public static let carrotYellow = UIColor(named: "3. Yellow")
+    public static let carrotLightYellow = UIColor(named: "4. Light Yellow")
+    public static let carrotBlue = UIColor(named: "5. Blue")
+    public static let carrotSkyBlue = UIColor(named: "6. Sky Blue")
+    public static let carrotLightSkyBlue = UIColor(named: "7. Light Sky Blue")
+    public static let carrotGreen = UIColor(named: "8. Green")
+    public static let carrotLightGreen = UIColor(named: "9. Light Green")
+    public static let carrotMint = UIColor(named: "10. Mint")
+    public static let carrotWhite = UIColor(named: "11. White")
+    public static let carrotGray1 = UIColor(named: "12. Gray1")
+    public static let carrotGray2 = UIColor(named: "13. Gray2")
+    public static let carrotGray3 = UIColor(named: "14. Gray3")
+    public static let carrotGray4 = UIColor(named: "15. Gray4")
+    public static let carrotGray5 = UIColor(named: "16. Gray5")
+    public static let carrotBlack = UIColor(named: "17. Black")
+}
