b082dc6722341d32d6177038dae660b89abdcdb7
정채은 <cce523@hanmail.net>
2023-05-16 15:51:28 +0900

    Add [#10] UILabel LetterSpacing extension 추가

--- SOPT-32nd-APP4-CarrotiOS/SOPT-32nd-APP4-CarrotiOS/Common/Extensions/UILabel+.swift
+++ SOPT-32nd-APP4-CarrotiOS/SOPT-32nd-APP4-CarrotiOS/Common/Extensions/UILabel+.swift
@@ -26,4 +26,12 @@ extension UILabel {
             self.textAlignment = .center
         }
     }
+    
+    func addCharacterSpacing(_ value: Double) {
+        let kernValue = self.font.pointSize * CGFloat(value)
+        guard let text = text, !text.isEmpty else { return }
+        let string = NSMutableAttributedString(string: text)
+        string.addAttribute(NSAttributedString.Key.kern, value: kernValue, range: NSRange(location: 0, length: string.length - 1))
+        attributedText = string
+    }
 }
