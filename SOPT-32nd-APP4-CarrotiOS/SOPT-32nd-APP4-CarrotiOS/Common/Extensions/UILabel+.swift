3d9945fe62231a1f314f95d5c32b223a682c3d25
정채은 <cce523@hanmail.net>
2023-05-16 16:28:15 +0900

    Add [#10] UILabel UnderLine extension 추가

--- SOPT-32nd-APP4-CarrotiOS/SOPT-32nd-APP4-CarrotiOS/Common/Extensions/UILabel+.swift
+++ SOPT-32nd-APP4-CarrotiOS/SOPT-32nd-APP4-CarrotiOS/Common/Extensions/UILabel+.swift
@@ -26,12 +26,21 @@ extension UILabel {
             self.textAlignment = .center
         }
     }
-
+    
     func addLetterSpacing(_ value: Double) {
         let kernValue = self.font.pointSize * CGFloat(value)
         guard let text = text, !text.isEmpty else { return }
         let string = NSMutableAttributedString(string: text)
         string.addAttribute(NSAttributedString.Key.kern, value: kernValue, range: NSRange(location: 0, length: string.length - 1))
         attributedText = string
+    }
+    
+    func underlineText() {
+        guard let labelText = text else { return }
+        
+        let attributedString = NSMutableAttributedString(string: labelText)
+        attributedString.addAttribute(.underlineStyle, value: NSUnderlineStyle.single.rawValue, range: NSMakeRange(0, attributedString.length))
+        
+        attributedText = attributedString
     }
 }
