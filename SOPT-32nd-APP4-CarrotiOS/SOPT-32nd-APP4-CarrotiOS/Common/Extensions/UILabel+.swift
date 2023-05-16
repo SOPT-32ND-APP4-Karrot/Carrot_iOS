eccaaee1efaed6ba802b361901bc7fd503744037
정채은 <cce523@hanmail.net>
2023-05-16 15:47:42 +0900

    Add [#10] UILabel Lineheight extension 추가

---
+++ SOPT-32nd-APP4-CarrotiOS/SOPT-32nd-APP4-CarrotiOS/Common/Extensions/UILabel+.swift
@@ -0,0 +1,29 @@
+//
+//  UILabel+.swift
+//  SOPT-32nd-APP4-CarrotiOS
+//
+//  Created by 정채은 on 2023/05/16.
+//
+
+
+import UIKit
+
+extension UILabel {
+    func setTextWithLineHeight(text: String?, lineHeight: CGFloat) {
+        if let text = text {
+            let style = NSMutableParagraphStyle()
+            style.maximumLineHeight = lineHeight
+            style.minimumLineHeight = lineHeight
+            
+            let attributes: [NSAttributedString.Key: Any] = [
+                .paragraphStyle: style,
+                .baselineOffset: (lineHeight - font.lineHeight) / 4
+            ]
+            
+            let attrString = NSAttributedString(string: text,
+                                                attributes: attributes)
+            self.attributedText = attrString
+            self.textAlignment = .center
+        }
+    }
+}
