142a6dd38e1ded07ee8decd4bc89762f59cb2478
정채은 <cce523@hanmail.net>
2023-05-26 14:57:58 +0900

    Feat [#39] HeaderView 추가

--- SOPT-32nd-APP4-CarrotiOS/SOPT-32nd-APP4-CarrotiOS/Presentation/Review/ReviewViewController.swift
+++ SOPT-32nd-APP4-CarrotiOS/SOPT-32nd-APP4-CarrotiOS/Presentation/Review/ReviewViewController.swift
@@ -7,8 +7,48 @@
 
 import UIKit
 
-class ReviewViewController: BaseViewController {
+import SnapKit
+import Then
 
+final class ReviewViewController: UIViewController {
     
+    let headerView = HeaderView()
+    
+    lazy var headerViewTitle = UILabel().then {
+        $0.font = .title
+        $0.addLineHeight(lineHeight: 28)
+        $0.text = "거래후기"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setStyle()
+        setLayout()
+    }
+}
 
+extension ReviewViewController {
+    private func setStyle() {
+        view.backgroundColor = .white
+        navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+    
+    private func setLayout() {
+        view.addSubviews(
+            headerView
+        )
+        headerView.addSubviews(headerViewTitle)
+        
+        headerView.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(44)
+            $0.horizontalEdges.equalTo(self.view.safeAreaLayoutGuide)
+            $0.height.equalTo(44)
+        }
+        
+        headerViewTitle.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.centerX.equalToSuperview()
+        }
+    }
 }
